492f99b0216e3de43a7d453fbdda91b74663552e
Shees <153302310+frasheees@users.noreply.github.com>
2024-02-01 13:00:27 +0100

    Add files via upload

---
+++ atmrobbery/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version "cerulean"
+author 'Shees X FiveM'
+description 'Nopixel inspired atm robbery'
+game "gta5"
+
+lua5.4 "yes"
+
+shared_script "config.lua"
+client_script "client/cl_main.lua"
+server_script "server/sv_main.lua"
+
+escrow_ignoe 'config.lua'
